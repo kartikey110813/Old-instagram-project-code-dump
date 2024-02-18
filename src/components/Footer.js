48551498aabd27414922c5899634814ac8875524
kartikey110813@gmail.com <kartikey110813@gmail.com>
2024-02-18 13:25:18 +0530

    Code dump

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { HeartTwoTone } from "@ant-design/icons";
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer">
+        Made with <HeartTwoTone twoToneColor="#eb2gc3" /> By{" "}
+        <a href="#">Kartikey tyagi</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Loader.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="sk-chase center_loader">
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+    </div>
+  );
+};
+
+export default Loader;
